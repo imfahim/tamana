55f6bc050c273b46a759a73c2a15fadfa5cfb466
Rafat Azad <rafat0819@gmail.com>
2018-02-01 12:48:39 +0530

    video

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -394,9 +394,9 @@
 				<div class="testimonial-opact testimonial-opact-v-two">
 					<div class="testimonial-containt">
 						<div class="container">
-							<div class="testimonial-shape-border"><i class="flaticon-right-quotation-sign"></i></div>
+							<div class="testimonial-shape-border"><h2 style="align: center;">VIDEO</h2></div>
 
-							<div id="client-carousel" class="carousel slide" data-ride="carousel" data-interval="2500">
+							<div id="" class="carousel slide" data-ride="carousel" data-interval="2500">
 								<!-- Indicators -->
 								<ol class="carousel-indicators">
 									<li data-target="#client-carousel" data-slide-to="0"></li>
@@ -406,21 +406,10 @@
 
 								<!-- Wrapper for slides -->
 								<div class="carousel-inner" role="listbox">
-								    <div class="item">
-								     	<p>Every day we bring real hope to millions of children in the world's hardest places as a sign of God's unconditional love.Every day we bring real hope to millions of children in the world's hardest places</p>
-								     	<span>Mahfuz R  ( CEO Ch )</span>
-								     	<img src="{{ asset('images/home/1.jpg')}}" alt="image">
-								    </div> <!-- /.item -->
 								    <div class="item active">
-								     	<p>Every day we bring real hope to millions of children in the world's hardest places as a sign of God's unconditional love.Every day we bring real hope to millions of children in the world's hardest places</p>
-								     	<span>Mahfuz R  ( CEO Ch )</span>
-								     	<img src="{{ asset('images/home/1.jpg')}}" alt="image">
+								     	<iframe width="854" height="480" src="https://www.youtube.com/embed/kn9DfZUTcIg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 								    </div> <!-- /.item -->
-								    <div class="item">
-								    	<p>Every day we bring real hope to millions of children in the world's hardest places as a sign of God's unconditional love.Every day we bring real hope to millions of children in the world's hardest places</p>
-								     	<span>Mahfuz R  ( CEO Ch )</span>
-								     	<img src="{{ asset('images/home/1.jpg')}}" alt="image">
-								    </div> <!-- /.item -->
+										
 								</div> <!-- /.carousel-inner -->
 							</div> <!-- Wrapper for bootstrap slides -->
 						</div> <!-- /.container -->
