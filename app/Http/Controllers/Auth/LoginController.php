f17b4178cf705feae10efa314920bd423dabe8a6
Fahim Uddin <raydvard@gmail.com>
2018-02-21 15:57:50 +0530

    Login #1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/webpanel';
 
     /**
      * Create a new controller instance.
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
+<br><br>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -66,4 +67,5 @@
         </div>
     </div>
 </div>
+<br><br>
 @endsection
